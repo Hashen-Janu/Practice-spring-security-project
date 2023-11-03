db23cde3de57a7edb007f3a5f9cd07a33ea242d9
Hashen-Janu <paranamanajanu@gmail.com>
2023-11-03 21:23:45 +0530

    first commit

---
+++ practiceSpringSecurity/src/main/java/Janu/practiceSpringSecurity/PracticeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package Janu.practiceSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ practiceSpringSecurity/src/test/java/Janu/practiceSpringSecurity/PracticeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Janu.practiceSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
